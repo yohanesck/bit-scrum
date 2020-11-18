d1cddcc82d0afcc99f840e86121cb13f54f85f7d
yohanesck <yohanes.chris@ti.ukdw.ac.id>
2020-11-18 21:57:55 +0700

    Employee By Name

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,8 @@ use Illuminate\Http\Request;
 Route::get('/', 'UserController@index');
 
 Route::get('/employee/name', 'EmployeeController@showName');
+Route::get('/building/{building}/floor/{floor}/employee', 'EmployeeController@getEmployeeByFloorBuilding');
 Route::get('/employee/{employee}/coordinate', 'EmployeeController@getEmployeeCoordinate');
 Route::get('/employee/{employee}', 'EmployeeController@show');
 Route::get('/employee', 'EmployeeController@getByName');
 Route::get('/floor', 'SeatController@getFloor');
-Route::get('/{building}/{floor}/employee', 'EmployeeController@getEmployeeByFloorBuilding');
