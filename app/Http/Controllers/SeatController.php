1b1e2c20dd5aeae654588641b96ee3e66ee69a84
yohanesck <yohanes.chris@ti.ukdw.ac.id>
2020-11-19 12:46:44 +0700

    Add A

--- app/Http/Controllers/SeatController.php
+++ app/Http/Controllers/SeatController.php
@@ -25,7 +25,7 @@ class SeatController extends Controller
 
     public function getDataSeatByFloor($building, $floor)
     {
-        dd(Seat::where('building_name', $building));
+        dd($this->seat->data());
         return response()->json([
             'result' => $this->seat->getDataByBuildingFloor($building, $floor)
         ], 200);
